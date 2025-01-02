a76bd5fc23f38b2673bd1d9a4a0adc273d6dc52b
takehiro1111 <t.soccer.0916@gmail.com>
2025-01-02 10:31:06 +0900

    hello world

---
+++ tutorial/src/if/if.js
@@ -0,0 +1,5 @@
+var num = 100;
+if (num > 10) {
+    console.log("".concat(num, "\u306F10\u3088\u308A\u5927\u304D\u3044\u3067\u3059\u3002"));
+}
+console.log("処理の完了");
---
+++ tutorial/src/if/if.ts
@@ -0,0 +1,9 @@
+const num = 100
+
+if(num > 10){
+  console.log(`${num}は10より大きいです。`)
+}
+
+console.log("処理の完了");
+
+
